905da2c0f60611a4c07f10b0e98590cb747cbc36
David952 <david.amorim@live.fr>
2022-10-05 12:49:21 +0200

    Initialisation "Create React App" "React Router"

---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Header from './components/Header'
+ 
+ReactDOM.render(
+    <React.StrictMode>
+        <Router>
+            <Header />
+            <Route exact path="/">
+                <Home />
+            </Route>
+            <Route path="/about">
+                <About />
+            </Route>
+        </Router>
+    </React.StrictMode>,
+document.getElementById('root')
+)
