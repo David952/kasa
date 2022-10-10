52e970356abe46ccd1c4f8cf18967b3ce265f9bf
David952 <david.amorim@live.fr>
2022-10-10 23:53:24 +0200

    MàJ 'About.jsx', 'Home.jsx' & import 'Error.jsx'

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,31 @@
+import React from "react";
+
+import Header from "../components/Header";
+import Banner from "../components/Banner";
+import Collapse from "../components/Collapse/About";
+import Footer from "../components/Footer";
+
+import image from "../assets/Banner/About/kalen-emsley-Bkci_8qcdvQ-unsplash.png"
+
+
 function About() {
     return (
-        <div>
-            <h1>À Propos</h1>
-        </div>
-    )
+        <React.Fragment>
+            <Header />
+            <Banner 
+                BannerClassImage='banner__image-About'
+                img={image}
+                alt='Montagne'
+            />
+            <main className="About__container">
+                <Collapse 
+                    CollapseTitle='Fiabilité'
+                    CollapseText='Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.'
+                />
+            </main>
+            <Footer />
+        </React.Fragment>
+    );
 }
 
-export default About
+export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,29 @@
-import '../styles/Home.css';
+import React from "react";
+
+import Header from "../components/Header";
+import Banner from "../components/Banner";
+import Card from "../components/Card";
+import Footer from "../components/Footer";
+
+import image from "../assets/Banner/Home/eric-muhr-P_XxsdVgtpQ-unsplash.png"
+
 
 function Home() {
   return (
-    <div className="App">
-      <h1>Kasa</h1>
-    </div>
+    <React.Fragment>
+        <Header />
+        <Banner 
+          BannerClassImage='banner__image-Home'
+          img={image}
+          alt='Bord de falaise'
+          BannerClassText='banner__text-Home'
+          BannerText='Chez vous, partout et ailleurs'
+        />
+        <main className="Home__container">
+            <Card />
+        </main>
+        <Footer />
+    </React.Fragment>
   );
 }
 
