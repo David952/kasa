75f93e3ddf4802cf9338a2756e93d8be2ac1e33b
David952 <david.amorim@live.fr>
2022-10-13 00:12:24 +0200

    Correction de 'About.jsx' (Collapse)

--- src/components/Collapse/About.jsx
+++ src/components/Collapse/About.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
+
 function Collapse(props) {
+
+    const [isOpen, setOpen] = useState(false);
+
     return (
         <div className='collapse'>
-            <div className="collapse__title_chevron">
+            <div className={`collapse__title_chevron ${isOpen ? "open" : "closed"}`} onClick={() => setOpen(!isOpen)}>
                 <h1 className="collapse__title">{props.CollapseTitle}</h1>
-                <span className="collapse__chevron"></span>
-            </div>        
-            <p className="collapse__text">{props.CollapseText}</p>
+                <span className={`collapse__chevron ${isOpen ? "open" : "closed"}`} onClick={() => setOpen(!isOpen)}></span>
+            </div>     
+            <div className={`collapse__container ${!isOpen ? "closed" && props.CollapseIsOpen : "open"}`}>
+            <p className="collapse__container-text">{props.CollapseText}</p>
+            </div>
         </div>
     );
 }
