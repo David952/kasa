f4d81cf7136060911e747c95161f7cb843067c02
David952 <david.amorim@live.fr>
2022-10-13 00:17:18 +0200

    Modification de 'About.jsx' (pages)

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,6 +9,30 @@ import image from "../assets/Banner/About/kalen-emsley-Bkci_8qcdvQ-unsplash.png"
 
 
 function About() {
+    
+    const dataCollapse = [
+        {
+            AboutTitle: 'Fiabilité',
+            AboutText: 'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.',
+            isOpen: 'closed'
+        },
+        {
+            AboutTitle: 'Respect',
+            AboutText: 'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
+            isOpen: 'open'
+        },
+        {
+            AboutTitle: 'Service',
+            AboutText: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+            isOpen: 'closed'
+        },
+        {
+            AboutTitle: 'Sécurité',
+            AboutText: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+            isOpen: 'closed'
+        },
+    ];
+    
     return (
         <React.Fragment>
             <Header />
@@ -18,10 +42,17 @@ function About() {
                 alt='Montagne'
             />
             <main className="About__container">
-                <Collapse 
-                    CollapseTitle='Fiabilité'
-                    CollapseText='Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.'
-                />
+                {
+                    dataCollapse && dataCollapse.map( data => {
+                        return (
+                            <Collapse 
+                                CollapseIsOpen={data.isOpen}
+                                CollapseTitle={data.AboutTitle}
+                                CollapseText={data.AboutText}
+                            />
+                        )
+                    })
+                }
             </main>
             <Footer />
         </React.Fragment>
