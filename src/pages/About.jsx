fca26a2510e40bacae51271129e6d7b44684a651
David952 <david.amorim@live.fr>
2022-10-14 23:55:04 +0200

    Correction des fichiers (pages)

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,62 +1,56 @@
-import React from "react";
+import Header from '../components/Header';
+import Banner from '../components/Banner';
+import Collapse from '../components/About/Collapse';
+import Footer from '../components/Footer';
 
-import Header from "../components/Header";
-import Banner from "../components/Banner";
-import Collapse from "../components/Collapse/About";
-import Footer from "../components/Footer";
-
-import image from "../assets/Banner/About/kalen-emsley-Bkci_8qcdvQ-unsplash.png"
-
+import image from '../assets/Banner/About/kalen-emsley-Bkci_8qcdvQ-unsplash.png';
 
 function About() {
-    
-    const dataCollapse = [
-        {
-            AboutTitle: 'Fiabilité',
-            AboutText: 'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.',
-            isOpen: 'closed'
-        },
-        {
-            AboutTitle: 'Respect',
-            AboutText: 'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
-            isOpen: 'open'
-        },
-        {
-            AboutTitle: 'Service',
-            AboutText: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
-            isOpen: 'closed'
-        },
-        {
-            AboutTitle: 'Sécurité',
-            AboutText: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
-            isOpen: 'closed'
-        },
-    ];
-    
-    return (
-        <React.Fragment>
-            <Header />
-            <Banner 
-                BannerClassImage='banner__image-About'
-                img={image}
-                alt='Montagne'
-            />
-            <main className="About__container">
-                {
-                    dataCollapse && dataCollapse.map( data => {
-                        return (
-                            <Collapse 
-                                CollapseIsOpen={data.isOpen}
-                                CollapseTitle={data.AboutTitle}
-                                CollapseText={data.AboutText}
-                            />
-                        )
-                    })
-                }
-            </main>
-            <Footer />
-        </React.Fragment>
-    );
+  const dataCollapse = [
+    {
+      AboutTitle: 'Fiabilité',
+      AboutText:
+        'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.',
+    },
+    {
+      AboutTitle: 'Respect',
+      AboutText:
+        'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
+    },
+    {
+      AboutTitle: 'Service',
+      AboutText:
+        "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+    },
+    {
+      AboutTitle: 'Sécurité',
+      AboutText:
+        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+  ];
+
+  return (
+    <>
+      <Header />
+      <Banner
+        BannerClassImage="banner__image-About"
+        img={image}
+        alt="Montagne"
+      />
+      <main className="about">
+        {dataCollapse &&
+          dataCollapse.map((data) => {
+            return (
+              <Collapse
+                CollapseTitle={data.AboutTitle}
+                CollapseText={data.AboutText}
+              />
+            );
+          })}
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,39 @@
-import React from "react";
+import { useLoaderData } from 'react-router-dom';
+import { getHousings } from './Housing/housings';
 
-import Header from "../components/Header";
-import Banner from "../components/Banner";
-import Card from "../components/Card";
-import Footer from "../components/Footer";
+import Header from '../components/Header';
+import Banner from '../components/Banner';
+import Card from '../components/Card';
+import Footer from '../components/Footer';
 
-import image from "../assets/Banner/Home/eric-muhr-P_XxsdVgtpQ-unsplash.png"
+import image from '../assets/Banner/Home/eric-muhr-P_XxsdVgtpQ-unsplash.png';
 
+export function loader() {
+  const housings = getHousings();
+  return { housings };
+}
 
 function Home() {
+  const { housings } = useLoaderData();
+
   return (
-    <React.Fragment>
-        <Header />
-        <Banner 
-          BannerClassImage='banner__image-Home'
-          img={image}
-          alt='Bord de falaise'
-          BannerClassText='banner__text-Home'
-          BannerText='Chez vous, partout et ailleurs'
-        />
-        <main className="Home__container">
-            <Card />
-        </main>
-        <Footer />
-    </React.Fragment>
+    <>
+      <Header />
+      <Banner
+        BannerClassImage="banner__image-Home"
+        img={image}
+        alt="Bord de falaise"
+        BannerClassText="banner__text-Home"
+        BannerText="Chez vous, partout et ailleurs"
+      />
+      <main className="home">
+        {housings &&
+          housings.map((housing) => {
+            return <Card housing={housing} key={housing.id} />;
+          })}
+      </main>
+      <Footer />
+    </>
   );
 }
 
