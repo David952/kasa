5ccc643f02529165af00cf19dd8ce8850d43ae8e
David952 <david.amorim@live.fr>
2022-11-03 14:42:01 +0100

    Correction du 'Slideshow.jsx' (components)

--- src/components/Housing/Slideshow.jsx
+++ src/components/Housing/Slideshow.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
+import useImagePreloader from '../../hooks/imgPreload';
 
 export default function Carousel(props) {
   const [sliderPosition, setSliderPosition] = useState(0);
+  const { imagesPreloaded } = useImagePreloader(props.CarouselImages);
+  const length = props.CarouselImages.length;
 
   const prevSlide = () => {
     setSliderPosition(
@@ -23,19 +26,32 @@ export default function Carousel(props) {
     <div className="carousel">
       <div className="carousel__item">
         {props.CarouselImages.map((image, index) => (
-          <div className="carousel__image" key={index}>
+          <div className="carousel__container" key={index}>
             {index === sliderPosition && (
-              <img src={image} alt="Logement dans un carrousel" />
+              <>
+                <img
+                  className="carousel__container-image"
+                  src={image}
+                  alt="Logement dans un carrousel"
+                />
+                <p className="carousel__container-text">
+                  {index + 1} / {image}
+                </p>
+              </>
             )}
           </div>
         ))}
       </div>
-      <span className="carousel__arrows arrow-left" onClick={prevSlide}>
-        ❮
-      </span>
-      <span className="carousel__arrows arrow-right" onClick={nextSlide}>
-        ❯
-      </span>
+      {length > 1 && (
+        <>
+          <span className="carousel__arrows arrow-left" onClick={prevSlide}>
+            ❮
+          </span>
+          <span className="carousel__arrows arrow-right" onClick={nextSlide}>
+            ❯
+          </span>
+        </>
+      )}
     </div>
   );
 }
